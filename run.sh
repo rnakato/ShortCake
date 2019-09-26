80ef48730ab84c650cc273d8015964ea32d01f70
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2019-09-26 14:01:18 +0900

    add run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-#docker run -u root -it rnakato/singlecell_jupyter /bin/bash
+#!/bin/bash
+
 docker run --name notebook -p 8888:8888 \
        -v $(pwd):/home/jovyan \
        rnakato/singlecell_jupyter \
-       start-notebook.sh #\
-#       --NotebookApp.password='sha1:ddb766b28c2b:6b0ff3cf909a320a103b9df0587f7a1908887240'
+       start-notebook.sh
