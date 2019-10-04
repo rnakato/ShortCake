d18fb960d2781ea378dc35a33d1f148ad18f86b1
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2019-10-04 14:34:51 +0900

    2019/10/04

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ for tag in 1.0.0 latest
 do
 #    docker build -f Dockerfile.anaconda -t rnakato/singlecell:$tag .
 #    docker push rnakato/singlecell:$tag
-    docker build -f Dockerfile.jupyter -t rnakato/singlecell_jupyter:$tag .
+#    docker build -f Dockerfile.jupyter -t rnakato/singlecell_jupyter:$tag .
+    docker build -f Dockerfile -t rnakato/singlecell_jupyter:$tag .
     docker push rnakato/singlecell_jupyter:$tag
 done
