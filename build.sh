6eee85ef84a8f7e55be84f92cc733718fb389769
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2020-03-07 22:23:44 +0900

    add Dockerfiles

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
-for tag in 1.0.2 latest 
+for tag in 1.0.2 latest
 do
-    docker build -f Dockerfile -t rnakato/singlecell_jupyter:$tag .
+    docker build -f Dockerfile.jupyter -t rnakato/singlecell_jupyter:$tag .
     docker push rnakato/singlecell_jupyter:$tag
+done
+
+for tag in latest 18.04
+do
+    docker build -f Dockerfile.ubuntu -t rnakato/singlecell_ubuntu:$tag .
+    docker push rnakato/singlecell_jupyter:$tag
+done
+
+for tag in latest
+do
+    docker build -f Dockerfile.monocle3 -t rnakato/monocle3:$tag .
+    docker push rnakato/monocle3:$tag
 done
