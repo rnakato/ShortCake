c4ab38fac57d5e51f0b080a6b8194a6478646252
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2020-03-19 18:52:24 +0900

    add Dockerfile.dorowu

--- build.sh
+++ build.sh
@@ -1,16 +1,24 @@
-for tag in 1.0.2 latest
+for tag in #1.0.2 latest
 do
     docker build -f Dockerfile.jupyter -t rnakato/singlecell_jupyter:$tag .
     docker push rnakato/singlecell_jupyter:$tag
 done
 
-for tag in latest 18.04
+for tag in #latest #18.04
 do
     docker build -f Dockerfile.ubuntu -t rnakato/singlecell_ubuntu:$tag .
-    docker push rnakato/singlecell_jupyter:$tag
+#    docker push rnakato/singlecell_ubuntu:$tag
 done
 
-for tag in latest
+
+for tag in dorowu-bionic
+do
+    docker build -f Dockerfile.dorowu -t rnakato/singlecell_ubuntu:$tag .
+    docker push rnakato/singlecell_ubuntu:$tag
+done
+
+
+for tag in #latest
 do
     docker build -f Dockerfile.monocle3 -t rnakato/monocle3:$tag .
     docker push rnakato/monocle3:$tag
