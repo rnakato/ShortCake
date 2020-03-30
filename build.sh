91d261b3400f94a7e16bec36309ab4e5bfc2e2a1
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2020-03-30 11:45:21 +0900

    singlecell_jupyter:1.0.3

--- build.sh
+++ build.sh
@@ -1,20 +1,20 @@
-for tag in #1.0.2 latest
+for tag in 1.0.3 latest
 do
     docker build -f Dockerfile.jupyter -t rnakato/singlecell_jupyter:$tag .
     docker push rnakato/singlecell_jupyter:$tag
 done
 
-for tag in #latest #18.04
+for tag in #latest 18.04
 do
     docker build -f Dockerfile.ubuntu -t rnakato/singlecell_ubuntu:$tag .
-#    docker push rnakato/singlecell_ubuntu:$tag
+    docker push rnakato/singlecell_ubuntu:$tag
 done
 
 
-for tag in dorowu-bionic
+for tag in #dorowu-bionic
 do
-    docker build -f Dockerfile.dorowu -t rnakato/singlecell_ubuntu:$tag .
-    docker push rnakato/singlecell_ubuntu:$tag
+    docker build -f Dockerfile.dorowu -t rnakato/singlecell_ubuntu:$tag . --no-cache
+#    docker push rnakato/singlecell_ubuntu:$tag
 done
 
 
