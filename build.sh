9fe4eadbc82f70d1b146f3137dc2d97fe88b10a4
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2022-11-16 16:51:54 +0900

    2022.11

--- build.sh
+++ build.sh
@@ -1,24 +1,7 @@
-tag=2022.08.2
-docker tag rnakato/singlecell_jupyter:latest rnakato/singlecell_jupyter:$tag
+tag=2022.11
+docker tag rnakato/shortcake:latest rnakato/shortcake:$tag
 
 for tag in $tag latest
 do
     docker push rnakato/singlecell_jupyter:$tag
 done
-
-exit
-
-
-
-
-for tag in latest #2020.12
-do
-    docker build -f Dockerfile -t rnakato/singlecell_jupyter:$tag .
-    docker push rnakato/singlecell_jupyter:$tag
-done
-
-#for tag in #latest
-#do
-#    docker build -f Dockerfile.monocle3 -t rnakato/monocle3:$tag .
-#    docker push rnakato/monocle3:$tag
-#done
--- testrun.sh
+++ testrun.sh
@@ -1,4 +1,4 @@
 for tool in dyngen CellChat ComplexHeatmap Seurat SeuratDisk SeuratData Signac SAVERX MOFA2 FLOWMAPR stringi metacell cellassign scAI harmony MUDAN DoubletFinder liger monocle3 garnett ArchR chromVAR JASPAR2016 JASPAR2018 SingleCellSignalR SAVER ClusterR SCRABBLE splatter loomR singleCellNet scCATCH velocyto.R singleCellHaystack scImpute SingleR conos CoGAPS scran slingshot scRNAseq scTensor monocle scater BSgenome.Hsapiens.UCSC.hg19 BSgenome.Hsapiens.UCSC.hg38 BSgenome.Mmusculus.UCSC.mm10 celldex
 do
-    docker run -it --rm rnakato/singlecell_jupyter R -e "library("$tool")"
+    docker run -it --rm rnakato/shortcake R -e "library("$tool")"
 done
