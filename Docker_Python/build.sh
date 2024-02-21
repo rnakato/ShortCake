fac964407864d36e55a9f2060d046cc446b28c99
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2024-02-21 20:41:16 +0900

    Update v2.0.0

--- Docker_Python/build.sh
+++ Docker_Python/build.sh
@@ -1,6 +1,6 @@
 docker-compose -f docker-compose.celloracle.yml build #--no-cache
 docker-compose -f docker-compose.yml build #--no-cache
-docker-compose -f docker-compose.ver2.yml build #--no-cache
+#docker-compose -f docker-compose.ver2.yml build #--no-cache
 
 #reponame=shortcake
 #docker save -o $reponame.tar rnakato/$reponame
--- Docker_R/build.sh
+++ Docker_R/build.sh
@@ -1,2 +1,2 @@
-#docker compose -f docker-compose.R_Seurat.yml build #--no-cache
+docker compose -f docker-compose.R_Seurat.yml build #--no-cache
 docker compose -f docker-compose.R.yml build #--no-cache
