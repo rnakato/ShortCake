cc7a0094fb8f2760362dc56d5dc1844e248b9c73
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2024-11-25 21:12:42 +0900

    Update v3.1.0

--- Docker_R/build.sh
+++ Docker_R/build.sh
@@ -5,5 +5,9 @@ docker-compose -f docker-compose.yml build r
 for name in shortcake_seurat shortcake_r
 do
     tag=3.1.0
-    docker push rnakato/$name:$tag
+    docker tag rnakato/$name:$tag rnakato/$name:latest
+    for tag in $tag latest
+    do
+        docker push rnakato/$name:$tag
+    done
 done
