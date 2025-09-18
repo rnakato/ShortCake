17f2fa829781b4c1764fa2f39c0c657e33ef409f
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2025-09-18 21:15:05 +0900

    Update v3.4.0

--- Docker_single_tools/build.sh
+++
@@ -1,9 +0,0 @@
-tag=202310
-for label in stellar-gears-saturn #scvi-scgen #dictys
-do
-    imagename=shortcake_$label
-    docker build -f Dockerfile.$label -t rnakato/$imagename .
-    docker push rnakato/$imagename
-    docker tag shortcake_$label:$tag shortcake_$label
-    docker push rnakato/$imagename:$tag
-done
