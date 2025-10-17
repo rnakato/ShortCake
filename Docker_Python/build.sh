ac53b46e268694c81f66724bf28f3ca445d9c463
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2025-10-17 11:05:05 +0900

    Update v3.4.0

--- Docker_Python/build.sh
+++ Docker_Python/build.sh
@@ -8,20 +8,17 @@
 #exit
 reponame=shortcake_light
 tag=3.4.0
-docker tag rnakato/$reponame:$tag rnakato/$reponame:latest
-apptainer build -F /work/SingularityImages/$reponame.$tag.sif docker-daemon://rnakato/$reponame:$tag
+#docker tag rnakato/$reponame:$tag rnakato/$reponame:latest
+#apptainer build -F /work/SingularityImages/$reponame.$tag.sif docker-daemon://rnakato/$reponame:$tag
 
 
 #docker save -o $reponame.tar rnakato/$reponame
 #singularity build -F $reponame.sif docker-archive://$reponame.tar
 
-for name in #shortcake_light #shortcake # shortcake_full shortcake_scvi shortcake_rapidsc
+for name in shortcake_light shortcake shortcake_full shortcake_scvi shortcake_rapidsc
 do
     tag=3.4.0
-#    docker tag rnakato/$name:$tag rnakato/$name:latest
-#    docker save -o $name-$tag.tar rnakato/$name:$tag
-#    singularity build -F /work3/SingularityImages/$name.$tag.sif docker-archive://$name-$tag.tar
-    exit
+    docker tag rnakato/$name:$tag rnakato/$name:latest
     for tag in $tag latest
     do
          docker push rnakato/$name:$tag
